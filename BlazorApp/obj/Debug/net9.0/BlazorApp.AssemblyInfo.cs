8aab66a99905698db39bd8dff56a5caad7251eb4
Teekay1702 <tokoloho57@gmail.com>
2025-04-04 18:00:51 +0200

    Updated the home and counter pages to increment by 10

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ddb10dffa50331ae540c54c0bf87ff64533dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b597a6392f8f559feed467f00b2d3eaa4345b24a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
